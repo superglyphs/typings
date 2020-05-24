da976dd325aed1a70b300e2819a83e5e607cb31b
todorpanev <toshopanev@gmail.com>
2020-05-24 16:59:06 +0300

    counter implementation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { DictionaryService } from './dictionary.service'
+import { take } from 'rxjs/operators'
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,117 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+
+  constructor(private DictionaryService: DictionaryService) {
+   }
+
   title = 'type-test-ng';
+  randomString: string;
+  score = {
+    yay: 0,
+    nay: 0
+  };
+  isLetterCorrect: Boolean;
+  letters: string[] = [];
+  dict: string[];
+  time: number = 10;
+  opacity: number = 100;
+  bars: string[] = [''];
+
+  typeLetter() {
+        
+    document.addEventListener('keydown', (e) => {
+      
+      if (e.key.length === 1) {
+        this.letters.push(e.key);
+      }
+
+      if (e.key === "Backspace") {
+        this.letters.pop();
+
+      }
+
+      if (e.key === "Enter") {
+        this.checkInput();
+        this.refreshData();
+        this.resetTimer();
+
+      }
+      
+    })
+
+}
+
+refreshData() {
+  this.letters = [];
+  this.generateData();
+}
+
+generateData() {
+
+  let dataLenght = this.dict.length;
+
+  function getRandomInt(max: number) {
+    return Math.floor(Math.random() * Math.floor(max));
+  }
+
+  this.randomString = this.dict[getRandomInt(dataLenght)]
+
+
+}
+
+startTimer() {
+
+  this.resetTimer();
+
+  setInterval(() => {
+
+    this.time--;
+    this.opacity -= 0.1;
+    this.bars.pop();
+
+    if (this.time < 0) {
+      this.checkInput();
+      this.refreshData();
+      this.resetTimer();
+    }
+
+  }, 1000)
+
+}
+
+resetTimer() {
+  this.time = 10;
+  this.opacity = 1;
+  this.bars.length = 10;
+}
+
+checkLetter(l: string, index: number) {
+  return this.randomString[index] === l
+}
+
+checkInput() {
+
+  // stringify letters array
+  let inputString = this.letters.join("")
+
+  // check if input matches random data
+  if (inputString === this.randomString) {
+    this.score.yay++;
+  }
+  else {
+    this.score.nay++;
+  }
+
+}
+
+  ngOnInit() {
+    this.DictionaryService.getDict().pipe(take(1)).subscribe((data)=> {
+      this.dict = data;
+      this.generateData();
+    })
+    this.typeLetter();
+    this.startTimer();
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppComponent } from './app.component';
+import { HttpClientModule } from '@angular/common/http';
+import { LetterComponent } from './letter/letter.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LetterComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
   ],
-  providers: [],
+  providers: [
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
